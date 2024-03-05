fe5961ce93442a351a23cfacce9fcf8444639084
shruthi reddy <shruthireddy583@gmail.com>
2024-03-05 14:36:40 +0530

    rs

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import Header from"./Components/Header"
+import Footer from"./Components/Footer"
+import Message from"./Components/Message"
+
+function App(){
+    return<div>
+        <h1>App Components</h1>
+        <Header/>
+        <Footer/>
+        <Message/>
+    </div>
+}
+export default App
---
+++ my-app/src/Components/Message.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+
+class Message extends Component{
+    render(){
+        return<div>
+            <h3>Message Components</h3>
+        </div>
+    }
+}
+export default Message
