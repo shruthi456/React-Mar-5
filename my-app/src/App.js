e02200fc6550baa085faf5fd1dbd32a550dab311
shruthi reddy <shruthireddy583@gmail.com>
2024-03-06 15:39:13 +0530

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Movies from './Movies/Movies'
+import Navbar from './Navbar/Navbar'
 function App(){
     return<div>
         <Movies/>
+        <Navbar/>
     </div>
 }
 export default App
